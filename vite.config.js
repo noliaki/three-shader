0fac18a3c579f0d43a65443f060f3a64b6442138
noliaki <noliaki@gmail.com>
2021-06-29 23:05:36 +0900

    add index

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ module.exports = {
   build: {
     rollupOptions: {
       input: {
+        index: resolve(__dirname, 'index.html'),
         melt: resolve(__dirname, 'melt/index.html'),
         triangle: resolve(__dirname, 'triangle/index.html'),
       },
